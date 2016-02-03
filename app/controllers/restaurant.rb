772ce243f8290a482588bcfcf992b9312845c9b1
iregina <reginawong14@gmail.com>
2016-02-02 16:14:11 -0800

    create form with front-end error messages

--- app/controllers/restaurant.rb
+++ app/controllers/restaurant.rb
@@ -1,5 +1,32 @@
 get '/restaurants' do
 	@restaurants = Restaurant.all
 	erb :"/restaurants/index"
+end
 
+get '/restaurants/new' do
+  @restaurant = Restaurant.new
+  if request.xhr?
+    erb :"/restaurants/new", layout: false
+  else
+    erb :"/restaurants/new"
+  end
+end
+
+post '/restaurants' do
+  @restaurant = Restaurant.new(params[:restaurant])
+  if request.xhr?
+    if @restaurant.save
+      status 200
+      erb :"/restaurants/_show", layout: false, locals: {restaurant: @restaurant}
+    else
+      status 422
+      erb :"/restaurants/new", layout: false
+    end
+  else
+    if @restaurant.save
+      redirect '/restaurants'
+    else
+      erb :"/restaurants/new"
+    end
+  end
 end
